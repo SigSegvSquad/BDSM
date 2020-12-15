64ecc11f7f411b27ee616587096d92423c261ff1
prabhav-pandya <business.prabhav@gmail.com>
2020-12-15 19:08:32 +0530

    BPlusTree Pushd

--- main.cpp
+++ main.cpp
@@ -12,21 +12,3 @@ int main() {
 
     return 0;
 }
-
-//int main()
-//{
-//    BPlusTree node;
-//
-//    // Create B+ Tree
-//    node.insert(6);
-//    node.insert(16);
-//    node.insert(26);
-//    node.insert(36);
-//    node.insert(46);
-//
-//    // Function Call to search node
-//    // with value 16
-//    node.search(16);
-//
-//    return 0;
-//}
