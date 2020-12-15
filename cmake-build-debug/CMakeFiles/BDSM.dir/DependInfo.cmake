f432a0cf9ae9d5a340533c97c8b5b0f9f564c46d
prabhav-pandya <business.prabhav@gmail.com>
2020-12-15 18:59:34 +0530

    BPTree Pushd

---
+++ cmake-build-debug/CMakeFiles/BDSM.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/vabha/Desktop/College/HA/BDSM/B+ Tree/BPlusNode.cpp" "C:/Users/vabha/Desktop/College/HA/BDSM/cmake-build-debug/CMakeFiles/BDSM.dir/B+_Tree/BPlusNode.cpp.obj"
+  "C:/Users/vabha/Desktop/College/HA/BDSM/B+ Tree/BPlusTree.cpp" "C:/Users/vabha/Desktop/College/HA/BDSM/cmake-build-debug/CMakeFiles/BDSM.dir/B+_Tree/BPlusTree.cpp.obj"
+  "C:/Users/vabha/Desktop/College/HA/BDSM/B Tree/BNode.cpp" "C:/Users/vabha/Desktop/College/HA/BDSM/cmake-build-debug/CMakeFiles/BDSM.dir/B_Tree/BNode.cpp.obj"
+  "C:/Users/vabha/Desktop/College/HA/BDSM/B Tree/BTree.cpp" "C:/Users/vabha/Desktop/College/HA/BDSM/cmake-build-debug/CMakeFiles/BDSM.dir/B_Tree/BTree.cpp.obj"
+  "C:/Users/vabha/Desktop/College/HA/BDSM/Database/Student.cpp" "C:/Users/vabha/Desktop/College/HA/BDSM/cmake-build-debug/CMakeFiles/BDSM.dir/Database/Student.cpp.obj"
+  "C:/Users/vabha/Desktop/College/HA/BDSM/main.cpp" "C:/Users/vabha/Desktop/College/HA/BDSM/cmake-build-debug/CMakeFiles/BDSM.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/BDSM.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "BDSM.exe"
+  "BDSM.exe.manifest"
+  "BDSM.pdb"
+  "CMakeFiles/BDSM.dir/B+_Tree/BPlusNode.cpp.obj"
+  "CMakeFiles/BDSM.dir/B+_Tree/BPlusTree.cpp.obj"
+  "CMakeFiles/BDSM.dir/B_Tree/BNode.cpp.obj"
+  "CMakeFiles/BDSM.dir/B_Tree/BTree.cpp.obj"
+  "CMakeFiles/BDSM.dir/Database/Student.cpp.obj"
+  "CMakeFiles/BDSM.dir/main.cpp.obj"
+  "libBDSM.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BDSM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
